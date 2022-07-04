1c72c2f42ae829acda78064fba41100c975e1658
OptimisticTrousers <tony.isern@yahoo.com>
2022-07-04 19:23:34 -0400

    Adding Skills component

---
+++ src/components/Skills.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Skills extends React.Component {
+  render() {
+    return (
+      <div className="skills">
+        <h2 className="skills-title">SKILLS</h2>
+        <ul classNam="skills-list">
+          <li className="skill">Design strategy</li>
+          <li className="skill">Web development</li>
+          <li className="skill">Bob jones</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Skills;
