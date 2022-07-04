a7fde3d20676b60c78f716f9a8e611b7845670ee
OptimisticTrousers <tony.isern@yahoo.com>
2022-07-04 19:04:52 -0400

    Creating Experience.js and .css files

---
+++ src/components/Experience.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+class Experience extends React.Component {
+  render() {
+    return (
+      <div className="experience">
+        <h2>PRACTICAL EXPERIENCE</h2>
+        <div className="experience-container">
+          <div className="position">
+            <h3>
+              Schibsted <span>Head of Design</span>
+            </h3>
+            <p>July 2021 - Present</p>
+          </div>
+          <p className="experience-description">
+            Adipisicing qui dolor duis officia nostrud laborum eu ea
+            consectetur. Enim mollit ut tempor qui mollit. Sint magna sit elit
+            officia exercitation nisi commodo in consectetur.
+          </p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Experience
