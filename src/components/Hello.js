ed347ee64663f7b47d60a12bcbba624b28f13f8a
OptimisticTrousers <tony.isern@yahoo.com>
2022-07-04 07:29:25 -0400

    Creating and adding boilerplate for Hello.js and Hello.css

---
+++ src/components/Hello.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Hello.css';
+
+class Hello extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Ok</h1>
+      </div>
+    );
+  }
+}
+
+export default Hello;
