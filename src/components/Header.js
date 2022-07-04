4c1127a75d04de2d8e546e45bb04335699993783
OptimisticTrousers <tony.isern@yahoo.com>
2022-07-04 06:46:52 -0400

    Removing react.js logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,24 @@
-import React from 'react'
-import './Header.css'
+import React from 'react';
+import './Header.css';
 
 class Header extends React.Component {
-    constructor(props){
-        super(props)
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    render() {
-        return (
-            <div className="header">
-                <div className="introduction">
-                    <p className="i-am">I AM</p>
-                    <h1>Tom Newton</h1>
-                    <p className="occupation">A Digital Designer based in London, England.</p>
-                </div>
-            </div>
-        )
-    }
-
+  render() {
+    return (
+      <div className="header">
+        <div className="introduction">
+          <p className="i-am">I AM</p>
+          <h1>Tom Newton</h1>
+          <p className="occupation">
+            A Digital Designer based in London, England.
+          </p>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Header
+export default Header;
