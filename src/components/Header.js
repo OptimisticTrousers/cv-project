b7694d2b390c95593037a6c442698b2e803e0156
OptimisticTrousers <tony.isern@yahoo.com>
2022-07-04 07:10:52 -0400

    Adding border to header and fixing align

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,10 +2,6 @@ import React from 'react';
 import './Header.css';
 
 class Header extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     return (
       <div className="header">
