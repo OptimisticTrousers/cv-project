bc32d3f5444e9c61ab0cd865722e3e4ecc800043
OptimisticTrousers <tony.isern@yahoo.com>
2022-07-04 11:29:22 -0400

    Centering image and text

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import './Hello.css';
+import profilePicture from '../images/optimistictrousers.jpg';
 
 class Hello extends React.Component {
   render() {
     return (
       <div className="hello-background">
-        <img src="../images/optimistictrousers.jpg" alt="applicant" />
-        <h2>Hello, I am Optimistic Trousers</h2>
-        <p>
-          Sunt exercitation aute sit amet consequat deserunt nostrud anim
-          consequat ea laborum aute. Est laboris magna consectetur esse eu sint
-          minim. Culpa exercitation amet sunt ex. Deserunt fugiat mollit ipsum
-          commodo ea nisi eu incididunt sit magna commodo in sint commodo.
-          Aliqua nostrud incididunt Lorem quis et.
-        </p>
+        <div className="hello">
+          <div className="image">
+            <img src={profilePicture} alt="applicant" />
+          </div>
+          <h2>Hello, I am Optimistic Trousers</h2>
+          <p>
+            Sunt exercitation aute sit amet consequat deserunt nostrud anim
+            consequat ea laborum aute. Est laboris magna consectetur esse eu
+            sint minim. Culpa exercitation amet sunt ex. Deserunt fugiat mollit
+            ipsum commodo ea nisi eu incididunt sit magna commodo in sint
+            commodo. Aliqua nostrud incididunt Lorem quis et.
+          </p>
+        </div>
       </div>
     );
   }
