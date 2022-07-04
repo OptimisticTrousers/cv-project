8fa1f291b02a075b1b129912293b0e2f70861e52
OptimisticTrousers <tony.isern@yahoo.com>
2022-07-04 12:13:05 -0400

    Adding boilerplate for Interests component

---
+++ src/components/Interests.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class Interests extends React.Component {
+    render(){
+        return (
+            <div class="interests">
+                <h2>THINGS I LIKE TO DO</h2>
+            </div>
+        )
+    }
+}
