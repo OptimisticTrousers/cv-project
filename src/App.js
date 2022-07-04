edd94a9a465b6fa3935b0e19b255132e49e14735
OptimisticTrousers <tony.isern@yahoo.com>
2022-07-04 12:31:10 -0400

    Adding boilerplate to Interests component and rendering it onto App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './components/Header';
 import Hello from './components/Hello';
+import Interests from './components/Interests';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Hello />
+      <Interests />
     </div>
   );
 }
--- src/components/Interests.js
+++ src/components/Interests.js
@@ -1,11 +1,63 @@
-import React from 'react'
+import React from 'react';
 
 class Interests extends React.Component {
-    render(){
-        return (
-            <div class="interests">
-                <h2>THINGS I LIKE TO DO</h2>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="interests">
+        <h2>THINGS I LIKE TO DO</h2>
+        <div className="container">
+          <div className="interest">
+            <img alt="monitor" />
+            <h3>RESPONSIVE WEB DESIGN</h3>
+            <p>
+              Sunt proident mollit qui ullamco. Nisi cillum consectetur pariatur
+              veniam consectetur quis ex occaecat pariatur dolor irure non
+              cillum. Incididunt elit elit laboris aliqua eiusmod excepteur ex
+              magna id est labore eiusmod ad laborum. Laboris ea culpa cillum
+              incididunt velit nostrud id ipsum esse aliqua pariatur labore
+              incididunt.
+            </p>
+          </div>
+          <div className="interest">
+            <img alt="webpage" />
+            <h3>WEB DEVELOPMENT</h3>
+            <p>
+              Sunt proident mollit qui ullamco. Nisi cillum consectetur pariatur
+              veniam consectetur quis ex occaecat pariatur dolor irure non
+              cillum. Incididunt elit elit laboris aliqua eiusmod excepteur ex
+              magna id est labore eiusmod ad laborum. Laboris ea culpa cillum
+              incididunt velit nostrud id ipsum esse aliqua pariatur labore
+              incididunt.
+            </p>
+          </div>
+          <div className="interest">
+            <img alt="document" />
+            <h3>PRINT DESIGN</h3>
+            <p>
+              Sunt proident mollit qui ullamco. Nisi cillum consectetur pariatur
+              veniam consectetur quis ex occaecat pariatur dolor irure non
+              cillum. Incididunt elit elit laboris aliqua eiusmod excepteur ex
+              magna id est labore eiusmod ad laborum. Laboris ea culpa cillum
+              incididunt velit nostrud id ipsum esse aliqua pariatur labore
+              incididunt.
+            </p>
+          </div>
+          <div className="interest">
+            <img alt="light-bulb" />
+            <h3>BRANDING</h3>
+            <p>
+              Sunt proident mollit qui ullamco. Nisi cillum consectetur pariatur
+              veniam consectetur quis ex occaecat pariatur dolor irure non
+              cillum. Incididunt elit elit laboris aliqua eiusmod excepteur ex
+              magna id est labore eiusmod ad laborum. Laboris ea culpa cillum
+              incididunt velit nostrud id ipsum esse aliqua pariatur labore
+              incididunt.
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
+
+export default Interests;
