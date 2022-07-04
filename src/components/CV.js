0c66ef417913d39d20e17ed87524ea82ff3226e9
OptimisticTrousers <tony.isern@yahoo.com>
2022-07-04 13:22:18 -0400

    Adding boilerplate for curriculum vitae component

---
+++ src/components/CV.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class CV extends React.Component {
+  render() {
+    return (
+      <div className="curriculum-vitae">
+        <div className="curriculum-vitae-container">
+          <h2>CURRICULUM VITAE</h2>
+          <div class="curriculum-vitae-grid">
+            <p></p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default CV;
