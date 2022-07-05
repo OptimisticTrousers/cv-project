ae8430c02c6aea24ff81a8d45e10c65840c36c3b
OptimisticTrousers <tony.isern@yahoo.com>
2022-07-05 06:38:37 -0400

    Dynamically adding skills

--- src/components/ui/Skills/Skills.js
+++ src/components/ui/Skills/Skills.js
@@ -1,14 +1,16 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
 
 class Skills extends React.Component {
   render() {
+    const { skills } = this.props;
     return (
       <div className="skills">
         <h2 className="skills-title">SKILLS</h2>
         <ul classNam="skills-list">
-          <li className="skill">Design strategy</li>
-          <li className="skill">Web development</li>
-          <li className="skill">Bob jones</li>
+          {skills.map((skill, index) => (
+            <li key={index}>{skill}</li>
+          ))}
         </ul>
       </div>
     );
