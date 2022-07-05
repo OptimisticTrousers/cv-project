599a2a95225c7d9f1495593b8025f32e7e054bd2
OptimisticTrousers <tony.isern@yahoo.com>
2022-07-05 09:28:52 -0400

    Rendering Profile

--- src/components/common/CV/CV.js
+++ src/components/common/CV/CV.js
@@ -4,6 +4,7 @@ import Experience from '../../ui/Experience/Experience';
 import Skills from '../../ui/Skills/Skills';
 import Qualifications from '../../ui/Qualifications/Qualifications';
 import Education from '../../ui/Education/Education';
+import Profile from '../Profile/Profile';
 
 const practicalExperiences = [
   {
@@ -72,38 +73,7 @@ class CV extends React.Component {
       <div className="curriculum-vitae">
         <div className="curriculum-vitae-container">
           <h2>CURRICULUM VITAE</h2>
-          <div className="head">
-            <h2>Optimistic Trousers</h2>
-            <div className="overview">
-              <p className="overview-description">
-                Ea minim aute nulla reprehenderit dolore mollit qui veniam dolor
-                sunt incididunt fugiat Lorem. Nisi culpa magna aute dolore
-                consectetur ea aliqua eiusmod amet. Voluptate in enim ipsum amet
-                id enim. Eiusmod mollit est pariatur ut do id occaecat aliqua
-                aliqua. Culpa et est pariatur nostrud. Aliquip sint et
-                exercitation sunt irure cupidatat duis. Ipsum non consectetur
-                quis reprehenderit ex.
-              </p>
-              <div className="contact">
-                <div className="contact-item">
-                  <p>Portfolio </p>
-                  <a>optimistictrousers.com</a>
-                </div>
-                <div className="contact-item">
-                  <p>Email</p>
-                  <a>theoptimistictrouser@protonmail.com</a>
-                </div>
-                <div className="contact-item">
-                  <p>Phone</p>
-                  <a>555-666-7777</a>
-                </div>
-                <div className="contact-item">
-                  <p>Location</p>
-                  <a>Earth</a>
-                </div>
-              </div>
-            </div>
-          </div>
+          <Profile />
           <div className="curriculum-vitae-grid">
             <Experience
               title="PRACTICAL EXPERIENCE"
--- src/components/common/Profile/Profile.js
+++ src/components/common/Profile/Profile.js
@@ -4,8 +4,36 @@ import './Profile.css';
 class Profile extends React.Component {
   render() {
     return (
-      <div>
-        <h1>ok</h1>
+      <div className="head">
+        <h2>Optimistic Trousers</h2>
+        <div className="overview">
+          <p className="overview-description">
+            Ea minim aute nulla reprehenderit dolore mollit qui veniam dolor
+            sunt incididunt fugiat Lorem. Nisi culpa magna aute dolore
+            consectetur ea aliqua eiusmod amet. Voluptate in enim ipsum amet id
+            enim. Eiusmod mollit est pariatur ut do id occaecat aliqua aliqua.
+            Culpa et est pariatur nostrud. Aliquip sint et exercitation sunt
+            irure cupidatat duis. Ipsum non consectetur quis reprehenderit ex.
+          </p>
+          <div className="contact">
+            <div className="contact-item">
+              <p>Portfolio </p>
+              <a>optimistictrousers.com</a>
+            </div>
+            <div className="contact-item">
+              <p>Email</p>
+              <a>theoptimistictrouser@protonmail.com</a>
+            </div>
+            <div className="contact-item">
+              <p>Phone</p>
+              <a>555-666-7777</a>
+            </div>
+            <div className="contact-item">
+              <p>Location</p>
+              <a>Earth</a>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
