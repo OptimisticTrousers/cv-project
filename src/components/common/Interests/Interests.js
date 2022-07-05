47a6aa7fd953a6f1ae205d04f4d0471ae40941b4
OptimisticTrousers <tony.isern@yahoo.com>
2022-07-04 20:49:10 -0400

    Adding an array of interest objects to render

--- src/components/common/Interests/Interests.js
+++ src/components/common/Interests/Interests.js
@@ -6,59 +6,63 @@ import contract from '../../../assets/contract.png';
 import branding from '../../../assets/branding.png';
 import Interest from '../../ui/Interest/Interest';
 
+const interests = [
+  {
+    image: monitor,
+    title: 'RESPONSIVE WEB DESIGN',
+    description: `
+    Sunt proident mollit qui ullamco. Nisi cillum consectetur pariatur
+    veniam consectetur quis ex occaecat pariatur dolor irure non
+    cillum. Incididunt elit elit laboris aliqua eiusmod excepteur ex
+    magna id est labore eiusmod ad laborum. Laboris ea culpa cillum
+    incididunt velit nostrud id ipsum esse aliqua pariatur labore
+    incididunt.
+    `,
+  },
+  {
+    image: website,
+    title: 'WEB DEVELOPMENT',
+    description: `
+    Sunt proident mollit qui ullamco. Nisi cillum consectetur pariatur
+    veniam consectetur quis ex occaecat pariatur dolor irure non
+    cillum. Incididunt elit elit laboris aliqua eiusmod excepteur ex
+    magna id est labore eiusmod ad laborum. Laboris ea culpa cillum
+    incididunt velit nostrud id ipsum esse aliqua pariatur labore
+    incididunt.
+    `,
+  },
+  {
+    image: contract,
+    title: 'PRINT DESIGN',
+    description: `
+    Sunt proident mollit qui ullamco. Nisi cillum consectetur pariatur
+    veniam consectetur quis ex occaecat pariatur dolor irure non
+    cillum. Incididunt elit elit laboris aliqua eiusmod excepteur ex
+    magna id est labore eiusmod ad laborum. Laboris ea culpa cillum
+    incididunt velit nostrud id ipsum esse aliqua pariatur labore
+    incididunt.
+    `,
+  },
+  {
+    image: branding,
+    title: 'BRANDING',
+    description: `
+    Sunt proident mollit qui ullamco. Nisi cillum consectetur pariatur
+    veniam consectetur quis ex occaecat pariatur dolor irure non
+    cillum. Incididunt elit elit laboris aliqua eiusmod excepteur ex
+    magna id est labore eiusmod ad laborum. Laboris ea culpa cillum
+    incididunt velit nostrud id ipsum esse aliqua pariatur labore
+    incididunt.
+    `,
+  },
+];
 class Interests extends React.Component {
   render() {
     return (
       <div className="interests">
         <h2>THINGS I LIKE TO DO</h2>
         <div className="interests-container">
-          <Interest
-            image={monitor}
-            title="RESPONSIVE WEB DESIGN"
-            description="Sunt proident mollit qui ullamco. Nisi cillum consectetur pariatur
-              veniam consectetur quis ex occaecat pariatur dolor irure non
-              cillum. Incididunt elit elit laboris aliqua eiusmod excepteur ex
-              magna id est labore eiusmod ad laborum. Laboris ea culpa cillum
-              incididunt velit nostrud id ipsum esse aliqua pariatur labore
-              incididunt."
-          />
-          <Interest
-            image={website}
-            title="WEB DEVELOPMENT"
-            description="
-              Sunt proident mollit qui ullamco. Nisi cillum consectetur pariatur
-              veniam consectetur quis ex occaecat pariatur dolor irure non
-              cillum. Incididunt elit elit laboris aliqua eiusmod excepteur ex
-              magna id est labore eiusmod ad laborum. Laboris ea culpa cillum
-              incididunt velit nostrud id ipsum esse aliqua pariatur labore
-              incididunt.
-          "
-          />
-          <Interest
-            image={contract}
-            title="PRINT DESIGN"
-            description="
-
-              Sunt proident mollit qui ullamco. Nisi cillum consectetur pariatur
-              veniam consectetur quis ex occaecat pariatur dolor irure non
-              cillum. Incididunt elit elit laboris aliqua eiusmod excepteur ex
-              magna id est labore eiusmod ad laborum. Laboris ea culpa cillum
-              incididunt velit nostrud id ipsum esse aliqua pariatur labore
-              incididunt.
-          "
-          />
-          <Interest
-            image={branding}
-            title="BRANDING"
-            description="
-              Sunt proident mollit qui ullamco. Nisi cillum consectetur pariatur
-              veniam consectetur quis ex occaecat pariatur dolor irure non
-              cillum. Incididunt elit elit laboris aliqua eiusmod excepteur ex
-              magna id est labore eiusmod ad laborum. Laboris ea culpa cillum
-              incididunt velit nostrud id ipsum esse aliqua pariatur labore
-              incididunt.
-          "
-          />
+          {interests.map((interest) => interest)}
         </div>
       </div>
     );
