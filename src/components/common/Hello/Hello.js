ce852a31402e6bcc621ca64fdd3304ddb0e1ae10
OptimisticTrousers <tony.isern@yahoo.com>
2022-07-06 12:21:00 -0400

    Allowing the user to place their own image in Hello.js

--- src/components/common/Hello/Hello.js
+++ src/components/common/Hello/Hello.js
@@ -11,9 +11,11 @@ class Hello extends React.Component {
       greeting: 'HELLO, I AM OPTIMISTIC TROUSERS',
       about:
         'Sunt exercitation aute sit amet consequat deserunt nostrud anim consequat ea laborum aute. Est laboris magna consectetur esse eu sint minim. Culpa exercitation amet sunt ex. Deserunt fugiat mollit ipsum commodo ea nisi eu incididunt sit magna commodo in sint commodo. Aliqua nostrud incididunt Lorem quis et. Amet sunt laborum officia fugiat. Nostrud consectetur laboris minim voluptate fugiat excepteur sit sint aliqua nisi eu cillum nostrud culpa. Enim incididunt est veniam laborum sit aute consectetur mollit.',
+      picture: profilePicture,
     };
 
     this.handleChange = this.handleChange.bind(this);
+    this.onImageChange = this.onImageChange.bind(this);
   }
 
   handleChange(event, value) {
@@ -22,15 +24,32 @@ class Hello extends React.Component {
     });
   }
 
+  onImageChange(event) {
+    if (event.target.files && event.target.files[0]) {
+      const img = event.target.files[0];
+      this.setState({
+        picture: URL.createObjectURL(img),
+      });
+    }
+  }
+
   render() {
-    const { greeting, about } = this.state;
+    const { greeting, about, picture } = this.state;
     const { conditionallyRender } = this.props;
-    const { handleChange } = this;
+    const { handleChange, onImageChange } = this;
     return (
       <div className="hello-background">
         <div className="hello">
           <div className="hello-image">
-            <img src={profilePicture} alt="applicant" />
+            {conditionallyRender(
+              <img src={picture} alt="applicant" />,
+              'input',
+              undefined,
+              'picture',
+              onImageChange,
+              undefined,
+              'file'
+            )}
           </div>
           <div className="inputs">
             {conditionallyRender(
