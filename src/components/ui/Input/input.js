d0540bb6c02fe484acd1dfcc332bfe8b8cf017f2
OptimisticTrousers <tony.isern@yahoo.com>
2022-07-11 17:11:42 -0400

    Changing the 'Input' component from a class to a functional component

--- src/components/ui/Input/input.js
+++ src/components/ui/Input/input.js
@@ -1,18 +1,14 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
 
-class Input extends React.Component {
-  render() {
-    const { value, handleChange, type } = this.props;
-    if (type === 'file') {
-      return <input type={type} onChange={handleChange} value={value} />;
-    }
-    return <input type={type} required onChange={handleChange} value={value} />;
+export default function Input(props) {
+  const { value, handleChange, type } = props;
+  if (type === 'file') {
+    return <input type={type} onChange={handleChange} value={value} />;
   }
+  return <input type={type} required onChange={handleChange} value={value} />;
 }
 
 Input.defaultProps = {
   type: 'text',
 };
-
-export default Input;
