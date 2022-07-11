03b50d473963fb8eaa5e1a8d82a5b53bb9c5e361
OptimisticTrousers <tony.isern@yahoo.com>
2022-07-11 17:17:15 -0400

    Changing the 'Header' component from a class to a functional component

--- src/components/common/Header/Header.js
+++ src/components/common/Header/Header.js
@@ -2,52 +2,41 @@
 import React from 'react';
 import './Header.css';
 
-class Header extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      name: 'Optimistic Trousers',
-      occupancy: 'A Software Engineer based in New York City, NY',
-    };
-
-    this.handleChange = this.handleChange.bind(this);
-  }
-
-  handleChange(event, value) {
-    this.setState({
+export default function Header(props) {
+  const [personDetails, setPersonDetails] = React.useState({
+    name: 'Optimistic Trousers',
+    occupancy: 'A Software Engineer based in New York City, NY',
+  });
+  function handleChange(event, value) {
+    setPersonDetails((prevState) => ({
+      ...prevState,
       [value]: event.target.value,
-    });
+    }));
   }
 
-  render() {
-    const { conditionallyRender } = this.props;
-    const { name, occupancy } = this.state;
-    const { handleChange } = this;
-    return (
-      <div className="header">
-        <div className="introduction">
-          <p className="i-am">I AM</p>
-          <div className="inputs">
-            {conditionallyRender(
-              <h1>{name}</h1>,
-              'input',
-              name,
-              'name',
-              handleChange
-            )}
-            {conditionallyRender(
-              <p className="occupation">{occupancy}</p>,
-              'input',
-              occupancy,
-              'occupancy',
-              handleChange
-            )}
-          </div>
+  const { conditionallyRender } = props;
+  const { name, occupancy } = personDetails;
+  return (
+    <div className="header">
+      <div className="introduction">
+        <p className="i-am">I AM</p>
+        <div className="inputs">
+          {conditionallyRender(
+            <h1>{name}</h1>,
+            'input',
+            name,
+            'name',
+            handleChange
+          )}
+          {conditionallyRender(
+            <p className="occupation">{occupancy}</p>,
+            'input',
+            occupancy,
+            'occupancy',
+            handleChange
+          )}
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
-
-export default Header;
